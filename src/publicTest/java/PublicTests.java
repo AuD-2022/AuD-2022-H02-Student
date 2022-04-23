f62f746556ac6e081c1983a90961e2bd2417bbb1
Oshgnacknak <osh@oshgnacknak.de>
2022-04-23 20:50:25 +0200

    Public Tests

---
+++ src/publicTest/java/PublicTests.java
@@ -0,0 +1,53 @@
+import h02.ElementWithIndex;
+import h02.ListOfArrays;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PublicTests {
+
+    @Test
+    void testConstructor() {
+        var list = new ListOfArrays<>(new Object[] {1, 2, 3, 4, 5});
+
+        assertIterableEquals(List.of(1, 2, 3, 4, 5), () ->
+            new ListOfArraysIteratorWrapper<>(list));
+    }
+
+    @Test
+    void testInsert() {
+        var list = new ListOfArrays<>(new Object[] {1, 2, 5});
+
+        list.insert(List.of(3, 4), 2);
+
+        assertIterableEquals(List.of(1, 2, 3, 4, 5), () ->
+            new ListOfArraysIteratorWrapper<>(list));
+    }
+
+    @Test
+    void testWithIterator() {
+        var list = new ListOfArrays<>(new Object[] {1, 3, 5});
+
+        list.insert(List.of(
+            new ElementWithIndex<Object>(2, 1),
+            new ElementWithIndex<Object>(4, 3)
+        ).iterator());
+
+        assertIterableEquals(List.of(1, 2, 3, 4, 5), () ->
+            new ListOfArraysIteratorWrapper<>(list));
+    }
+
+    @Test
+    void testExtract() {
+        var list = new ListOfArrays<>(new Object[] {1, 2, 3, 4, 5});
+        var extract = list.extract(2, 4);
+
+        assertIterableEquals(List.of(2, 3), () ->
+            new ListOfArraysIteratorWrapper<>(extract));
+
+        assertIterableEquals(List.of(1, 2, 5), () ->
+            new ListOfArraysIteratorWrapper<>(list));
+    }
+}
